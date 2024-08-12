430a9fa7ba13a6c8f92d98562e0a709007f15040
Trang <kaito2372000@gmail.com>
2024-08-12 09:57:35 +0900

    pj2

---
+++ src/main/java/com/example/ktech_spring_project2/KtechSpringProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.ktech_spring_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtechSpringProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtechSpringProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ktech_spring_project2/KtechSpringProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktech_spring_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtechSpringProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
